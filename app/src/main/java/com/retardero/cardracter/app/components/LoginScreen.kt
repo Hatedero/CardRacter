a8228e373157f74cdeaabe91ffab443ee4662e63
Hatedero <amaurymenneret@outlook.fr>
2025-09-16 14:18:42 +0200

    start

--- app/src/main/java/com/retardero/cardracter/app/components/LoginScreen.kt
+++ app/src/main/java/com/retardero/cardracter/app/components/LoginScreen.kt
@@ -1,28 +1,44 @@
 package com.retardero.cardracter.app.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Label
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.retardero.cardracter.ui.BaseButton
+import com.retardero.cardracter.ui.theme.CardRacterTheme
 
 @Destination
-//@Preview(showBackground = true)
 @Composable
-fun LoginScreen(navigator: DestinationsNavigator) {
+fun LoginScreen(navigator: DestinationsNavigator?) {
     Column (
-
+        modifier = Modifier.fillMaxSize()
     ) {
         Column (
 
         ) {
-            //FIELDS
+
+            TextField(
+                "email@email.com",
+                onValueChange = {}
+            )
         }
         Column (
 
         ) {
-            //BaseButton("Login", "")
+            BaseButton("Login", "")
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CardRacterTheme {
+        LoginScreen(null)
     }
 }
