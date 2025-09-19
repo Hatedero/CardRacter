51286091966ee4a2826fc795cb9214388b3a47ca
Hatedero <amaurymenneret@outlook.fr>
2025-09-19 12:03:29 +0200

    fix ui

--- app/src/main/java/com/retardero/cardracter/app/components/IndexScreen.kt
+++ app/src/main/java/com/retardero/cardracter/app/components/IndexScreen.kt
@@ -8,9 +8,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -33,16 +36,18 @@ fun IndexScreen(navigator: DestinationsNavigator) {
     Scaffold (
         bottomBar = {
             Row (
-                modifier = Modifier.padding(4.dp)
+                modifier = Modifier.padding(8.dp)
+                    .shadow(8.dp, RoundedCornerShape(8.dp))
             ) {
             NavBar()} },
         topBar = {
             Row (
-                modifier = Modifier.padding(4.dp)
+                modifier = Modifier.padding(8.dp)
+                    .shadow(8.dp, CircleShape)
             ) {
                 ProfileTab()
             }},
-        modifier = Modifier.padding(16.dp)
+        modifier = Modifier.padding(8.dp)
             .background(Background)
     ) {
             LazyVerticalStaggeredGrid(
