71bbc5d0dcf32dcd4babb96bc8f28b2cd6c91ea0
LucasDevan <lucas.devanlay@etu.univ-lyon1.fr>
2025-09-27 12:20:52 +0200

    Added additional files to separate functions in the AccoutnScreen file
    Added additional files to separate functions in the AccoutnScreen file
    
    AccountButton for the buttons : modifiy profile and log out
    
    AccoutnPicture to display the profile picture at large

---
+++ app/src/main/java/com/retardero/cardracter/ui/AccountButton.kt
@@ -0,0 +1,38 @@
+package com.retardero.cardracter.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.retardero.cardracter.ui.theme.Background
+import com.retardero.cardracter.ui.theme.Primary
+
+@Composable
+fun AccountButton(lable: String,navigator: DestinationsNavigator? = null){
+    Button(
+        onClick = { },
+        modifier = Modifier
+            .fillMaxWidth(0.7F)
+            .border(shape = RoundedCornerShape(20.dp),width=4.dp, color = Primary)
+        ,
+        colors = ButtonColors(
+            containerColor = Primary ,
+            contentColor = Color(red=0, green=0, blue=0),
+            disabledContainerColor = Background,
+            disabledContentColor = Primary
+        )
+    ) {
+        Text(
+            text=lable,
+            modifier = Modifier.background(Primary),
+        )
+    }
+}
