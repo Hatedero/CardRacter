da6d59882ce73a7e668f32275157f34fef8a1d78
LucasDevan <lucas.devanlay@etu.univ-lyon1.fr>
2025-12-18 17:31:10 +0100

    Added diferentiation between debug and release version of the app by adding a message in the nav bar and changing primary color to blue for debug

--- app/src/main/java/com/retardero/cardracter/ui/theme/Color.kt
+++ app/src/main/java/com/retardero/cardracter/ui/theme/Color.kt
@@ -1,8 +1,9 @@
 package com.retardero.cardracter.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import com.retardero.cardracter.BuildConfig.FLAVOR_ID
 
-val Primary = Color(0xFFBD4C86)
+val Primary = if (FLAVOR_ID == "debug") Color.Blue else Color(0xFFBD4C86)
 
 val PrimaryContainer = Color(0xFFBDA9B3)
 val Secondary = Color(189, 169, 179)
@@ -10,5 +11,5 @@ val Tertiary = Color(0xFF57392E)
 
 val Background = Color(0xfff4edde)
 val PrimaryBackground = Color(255,216,230)
-var darkBackground = Color(137, 74, 104)
+var darkBackground =  Color(137, 74, 104)
 val veryLightGrey = Color(234,234,234)
