b61a1b54806cd7f76d91d507361a51371b4a6bc2
LucasDevan <lucas.devanlay@etu.univ-lyon1.fr>
2025-12-18 09:40:00 +0100

    Filled in User DAO

--- app/src/main/java/com/retardero/cardracter/app/database/dao/userDAO.kt
+++ app/src/main/java/com/retardero/cardracter/app/database/dao/userDAO.kt
@@ -1,4 +1,29 @@
 package com.retardero.cardracter.app.database.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.retardero.cardracter.app.model.User
+
 interface userDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(user: User)
+
+    /*@Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(cards: List<Card>)*/
+
+    @Update
+    suspend fun updateUser(user:User)
+
+    @Query("SELECT * FROM user")
+    suspend fun getAllUsers(): List<User>
+
+    @Query("SELECT * FROM user WHERE id = :id")
+    suspend fun get(id : Int): User
+
+    @Delete
+    suspend fun delete(user:User)
 }
