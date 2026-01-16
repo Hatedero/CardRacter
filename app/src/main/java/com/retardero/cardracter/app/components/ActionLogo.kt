c543f3a165f6d93c0e092d96f6fa70720dffad3f
LucasDevan <lucas.devanlay@etu.univ-lyon1.fr>
2026-01-16 14:59:42 +0100

    Added ActionLogo function for easy re-use

---
+++ app/src/main/java/com/retardero/cardracter/app/components/ActionLogo.kt
@@ -0,0 +1,40 @@
+package com.retardero.cardracter.app.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.retardero.cardracter.destinations.AccountScreenDestination
+import com.retardero.cardracter.ui.theme.Primary
+
+@Composable
+fun ActionLogo(drawable : Int, toDo: () -> Unit){
+    Box(
+        modifier = Modifier.clip(CircleShape)
+        .background(Primary)
+        .width(80.dp)
+        .height(80.dp)
+        .clickable { toDo() }
+    ) {
+        Image(
+            modifier = Modifier
+                .clip(RoundedCornerShape(10000.dp))
+                .fillMaxSize().aspectRatio(1f),
+            painter = painterResource(drawable),
+            contentDescription = "Google signUp button",
+            contentScale = ContentScale.Crop,
+        )
+    }
+}
